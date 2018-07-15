6614abc4845e3f81da52477703d1af12e0804408
sholomka <sholomka@gmail.com>
2018-07-15 16:36:00 +0300

    master

--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -7,18 +7,20 @@ import "rxjs/add/observable/from";
 export class StaticDataSource {
     private products: Product[] = [
         new Product(1, "Product 1", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 2", "Category 2", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 3", "Category 3", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 4", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 5", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 6", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 7", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 8", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 9", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 10", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 11", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 12", "Category 1", "Product 1 (Category 1)", 100),
-        new Product(1, "Product 13", "Category 1", "Product 1 (Category 1)", 100),
+        new Product(1, "Product 2", "Category 1", "Product 2 (Category 1)", 100),
+        new Product(1, "Product 3", "Category 1", "Product 3 (Category 1)", 100),
+        new Product(1, "Product 4", "Category 1", "Product 4 (Category 1)", 100),
+        new Product(1, "Product 5", "Category 1", "Product 5 (Category 1)", 100),
+        new Product(1, "Product 6", "Category 2", "Product 6 (Category 2)", 100),
+        new Product(1, "Product 7", "Category 2", "Product 7 (Category 2)", 100),
+        new Product(1, "Product 8", "Category 2", "Product 8 (Category 2)", 100),
+        new Product(1, "Product 9", "Category 2", "Product 9 (Category 2)", 100),
+        new Product(1, "Product 10", "Category 3", "Product 10 (Category 3)", 100),
+        new Product(1, "Product 11", "Category 3", "Product 11 (Category 3)", 100),
+        new Product(1, "Product 12", "Category 3", "Product 12 (Category 3)", 100),
+        new Product(1, "Product 13", "Category 3", "Product 13 (Category 3)", 100),
+        new Product(1, "Product 14", "Category 3", "Product 14 (Category 3)", 100),
+        new Product(1, "Product 15", "Category 3", "Product 15 (Category 3)", 100),
     ];
 
     getProducts(): Observable<Product[]> {
